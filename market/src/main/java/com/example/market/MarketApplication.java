398c37e2ae94171dda1b71845cff5b6e781538f6
user <user@DESKTOP-6BUB7S3>
2024-04-01 16:02:27 +0900

    @Bean 추가

--- market/src/main/java/com/example/market/MarketApplication.java
+++ market/src/main/java/com/example/market/MarketApplication.java
@@ -2,10 +2,11 @@ package com.example.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MarketApplication {
-
+@Bean
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
 	}
