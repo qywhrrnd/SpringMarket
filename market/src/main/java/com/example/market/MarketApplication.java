6551d76e7f1c7b36412de475bc2dacf199405cbb
user <user@DESKTOP-O5OB743>
2024-04-02 13:39:24 +0900

    zzzz

--- market/src/main/java/com/example/market/MarketApplication.java
+++ market/src/main/java/com/example/market/MarketApplication.java
@@ -1,15 +1,30 @@
 package com.example.market;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class MarketApplication {
-@Bean
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
-		System.out.println("된다 안된다");
 	}
 
+	@Bean
+	SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		return bean.getObject();
+	}
+
+	@Bean
+	SqlSessionTemplate sqlSession(SqlSessionFactory factory) {
+		return new SqlSessionTemplate(factory);
+	}
 }
