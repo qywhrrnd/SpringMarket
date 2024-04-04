e533933359a59c4bd960e80a7a2b964dd9c72893
user <user@DESKTOP-O5OB743>
2024-04-04 09:46:49 +0900

    jjj

---
+++ springmarket/src/main/java/com/example/springmarket/Handler/WebSocketHandler.java
@@ -0,0 +1,33 @@
+package com.example.springmarket.Handler;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.example.springmarket.model.chat.ChatMessage;
+import com.example.springmarket.model.chat.ChatRoom;
+import com.example.springmarket.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("{}", payload);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        
+        ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+        chatRoom.handlerActions(session, chatMessage, chatService);
+    }
+}
